085309ca184a747438c6a43ae7a916b612bf8576
peterbandle <peterbandle@peters-air-6.fritz.box>
2018-02-01 11:24:25 +0100

    mdscna - fvandf

---
+++ ServerTest.ts
@@ -0,0 +1,58 @@
+// Node-Http-Modul importieren
+import * as Http from "http";
+// Node-Url-Modul importieren
+import * as Url from "url";
+
+namespace ServerTest {
+    // Neuer Datentyp AssocStringString: homogenes, assoziatives Array.
+    interface AssocStringString {
+        [key: string]: string;
+    }
+    
+    // Port vom Process-Objekt erfragen 
+    let port: number = process.env.PORT;
+    // Port nicht definiert -> lokale Maschine, Port selbst definieren
+    if (port == undefined)
+        port = 8100;
+    
+    // Server-Objekt kreieren
+    let server: Http.Server = Http.createServer();
+    // Event-Handler installieren
+    server.addListener("listening", handleListen);
+    server.addListener("request", handleRequest);
+    // Auf dem Port horchen
+    server.listen(port);
+
+    // Listening-Event: Rückmeldung wenn horchen läuft
+    function handleListen(): void {
+        console.log("Server listening on port " + port);
+    }
+
+    // Request-Event: Verarbeiten der Request und erstellen der Response
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("Ich höre Stimmen!!");
+        // Header: Antwort kommt im HTML-Format mit uft-8
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        // Header: ?
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        
+        // Response-Body
+        _response.write("Ich höre Stimmen!!<br>");
+        _response.write("Port: " + port + "<br>");
+        _response.write("Method: " + _request.method + "<br>");
+        _response.write("Url: " + _request.url + "<br>");
+        _response.write("Headers: " + _request.headers + "<br>");
+
+        // ?//bei Google Suche zeigt GitHub Seite Prof
+        let query: AssocStringString = Url.parse(_request.url, true).query;//was macht diese Zeile?
+        //übersetzt die URL in String
+        //Duden: as­so­zi­ie­ren - sich zusammenschließen, vereinigen; sich anschließen
+        //Daten werden als String in ein Stringarray abgelegt. Es ist also kein Array welches mit Zahlen aufgerufen wird
+        // ?
+        for (let key in query)//wie arbeitet diese Schleife?
+            console.log(key + ": " + query[key]);
+            //sie gibt die einzelnen Daten aus dem Stringarray aus
+        // Antwort abschließen und abschicken
+        _response.end();
+    }
+}
